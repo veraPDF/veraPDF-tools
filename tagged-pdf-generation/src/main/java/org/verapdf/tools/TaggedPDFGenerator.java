d0df7f07ec013dedc25460a3cc438fd5a7822620
Maxim <maxplusim@mail.ru>
2024-10-10 23:03:55 +0300

    Update tagged pdf generator

--- tagged-pdf-generation/src/main/java/org/verapdf/tools/TaggedPDFGenerator.java
+++ tagged-pdf-generation/src/main/java/org/verapdf/tools/TaggedPDFGenerator.java
@@ -4,8 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.*;
 import org.apache.pdfbox.pdfwriter.compress.CompressParameters;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
@@ -13,41 +12,25 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
 import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.PDStructureElement;
+import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.PDStructureNode;
 import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.PDStructureTreeRoot;
 import org.apache.pdfbox.pdmodel.documentinterchange.markedcontent.PDMarkedContent;
 import org.apache.pdfbox.pdmodel.documentinterchange.markedcontent.PDPropertyList;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName;
 
-import java.util.TreeSet;
+import java.util.Map;
 import java.util.logging.Logger;
 import java.util.HashMap;
-import java.util.Set;
 
 public class TaggedPDFGenerator {
-    private static Logger logger = Logger.getLogger("");
-    private static Set<String> types = new TreeSet<String>();
-    private static Set<String> typesExcludeTransit = new TreeSet<String>();
-    private static Set<String> transitionalTypes = new TreeSet<String>();
-    private static HashMap<String, String> files = new HashMap<String, String>();
+    private static final Logger LOGGER = Logger.getLogger(TaggedPDFGenerator.class.getCanonicalName());
+    private static final Map<StructureType, String> files = new HashMap<>();
     static {
-        transitionalTypes.add(StructureType.DIV.string());
-        transitionalTypes.add(StructureType.NON_STRUCT.string());
-        transitionalTypes.add(StructureType.PART.string());
-
-        for (StructureType type : StructureType.values()) {
-            types.add(type.string());
-            typesExcludeTransit.add(type.string());
-        }
-
-        for (String type : transitionalTypes) {
-            typesExcludeTransit.remove(type);
-        }
-
-        files.put(StructureType.DIV.string(), "transitionaltag_div_test");
-        files.put(StructureType.NON_STRUCT.string(), "transitionaltag_nonstruct_test");
-        files.put(StructureType.PART.string(), "transitionaltag_part_test");
-        files.put("all_inclusions", "all_inclusions_test");
+        files.put(StructureType.DIV, "transitionaltag_div_test");
+        files.put(StructureType.NON_STRUCT, "transitionaltag_nonstruct_test");
+        files.put(StructureType.PART, "transitionaltag_part_test");
+        files.put(null, "all_inclusions_test");
     }
     
     public static void main(String[] args) {
@@ -56,7 +39,7 @@ public class TaggedPDFGenerator {
         try {
             taggedPDFGenerator.run(args);
         } catch (Exception ex) {
-            logger.severe("Error during pdf generation: " + ex.getMessage() + ", proccess stopped.");
+            LOGGER.severe("Error during pdf generation: " + ex.getMessage() + ", proccess stopped.");
             ex.printStackTrace();
         }
     }
@@ -82,28 +65,31 @@ public class TaggedPDFGenerator {
 
         String parentFolder = getWorkingDir();
 
-        for (String type : files.keySet()) {
+        for (Map.Entry<StructureType, String> entry : files.entrySet()) {
+            StructureType type = entry.getKey();
             currentMCID = 1;
 
             PDDocument document;
-            if (type.equals("all_inclusions")) {
-                document = allInclusionsPDF(type);
+            if (type == null) {
+                document = allInclusionsPDF();
             } else {
                 document = transitionalPDF(type);
             }
 
-            File file = Paths.get(parentFolder).resolve(files.get(type) + ".pdf").toFile();
+            File file = Paths.get(parentFolder).resolve(entry.getValue() + ".pdf").toFile();
 
             document.save(file, CompressParameters.NO_COMPRESSION);
             document.close();
         }
     }
 
-    private PDDocument allInclusionsPDF(String pdftype) throws IOException {
+    private PDDocument allInclusionsPDF() throws IOException {
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
         PDDocumentCatalog catalog = document.getDocumentCatalog();
         PDStructureTreeRootAccess treeRoot = new PDStructureTreeRootAccess();
+        COSDictionary namespace2_0 = getPDF2_0Namespace(treeRoot);
+
 
         document.addPage(page);
         catalog.setStructureTreeRoot(treeRoot);
@@ -112,26 +98,22 @@ public class TaggedPDFGenerator {
 
         treeRoot.appendKid(textContent(content, " "));
         treeRoot.appendKid(textContent(content, " "));
-        for (String type : typesExcludeTransit) {
-            PDStructureElement element = new PDStructureElement(type, treeRoot);
-
+        for (StructureType type : StructureType.values()) {
+            if (type.isTransitional()) {
+                continue;
+            }
+            PDStructureElement element = createStructureElement(type, treeRoot, namespace2_0, page);
             element.appendKid(textContent(content, " "));
             element.appendKid(textContent(content, " "));
-            for (String subType : typesExcludeTransit) {
-                for (Integer index = 0; index < 2; index++) {
-                    PDStructureElement subElement = new PDStructureElement(subType, element);
-                    subElement.setPage(page);
-
-                    element.appendKid(subElement);
+            for (StructureType subType : StructureType.values()) {
+                if (subType.isTransitional()) {
+                    continue;
+                }
+                for (int index = 0; index < 2; index++) {
+                    createStructureElement(subType, element, namespace2_0, page);
                 }
             }
-
-            element.setPage(page);
-            treeRoot.appendKid(element);
-
-            element = new PDStructureElement(type, treeRoot);
-            element.setPage(page);
-            treeRoot.appendKid(element);
+            createStructureElement(type, treeRoot, namespace2_0, page);
         }
 
         content.close();
@@ -139,37 +121,29 @@ public class TaggedPDFGenerator {
         return document;
     }
 
-    private PDDocument transitionalPDF(String transitionalType) throws IOException {
+    private PDDocument transitionalPDF(StructureType transitionalType) throws IOException {
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
         PDDocumentCatalog catalog = document.getDocumentCatalog();
         PDStructureTreeRoot treeRoot = new PDStructureTreeRoot();
+        COSDictionary namespace2_0 = getPDF2_0Namespace(treeRoot);
 
         document.addPage(page);
         catalog.setStructureTreeRoot(treeRoot);
 
         PDPageContentStream content = new PDPageContentStream(document, page, AppendMode.OVERWRITE, false);
 
-        for (String type : types) {
-            PDStructureElement element = new PDStructureElement(type, treeRoot);
-            PDStructureElement transitionalElement = new PDStructureElement(transitionalType, element);
-
+        for (StructureType type : StructureType.values()) {
+            PDStructureElement element = createStructureElement(type, treeRoot, namespace2_0, page);
+            PDStructureElement transitionalElement = createStructureElement(transitionalType, element, namespace2_0, page);
             transitionalElement.appendKid(textContent(content, " "));
             transitionalElement.appendKid(textContent(content, " "));
-            for (String subType : types) {
-                for (Integer index = 0; index < 2; index++) {
-                    PDStructureElement sub_element = new PDStructureElement(subType, transitionalElement);
-                    sub_element.setPage(page);
-
-                    transitionalElement.appendKid(sub_element);
+            for (StructureType subType : StructureType.values()) {
+                for (int index = 0; index < 2; index++) {
+                    createStructureElement(subType, transitionalElement, namespace2_0, page);
                 }
             }
-
-            element.setPage(page);
-            element.appendKid(transitionalElement);
-
-            transitionalElement = new PDStructureElement(transitionalType, element);
-            element.appendKid(transitionalElement);
+            createStructureElement(transitionalType, element, namespace2_0, page);
             treeRoot.appendKid(element);
         }
 
@@ -191,5 +165,26 @@ public class TaggedPDFGenerator {
         content.endText();
 
         return markedContent;
+    }
+    
+    private static PDStructureElement createStructureElement(StructureType type, PDStructureNode parent,
+                                                             COSDictionary namespace2_0, PDPage page) {
+        PDStructureElement structureElement = new PDStructureElement(type.getType(), parent);
+        if (type.is2_0()) {
+            structureElement.getCOSObject().setItem(COSName.getPDFName("NS"), namespace2_0);
+        }
+        parent.appendKid(structureElement);
+        structureElement.setPage(page);
+        return structureElement;
+    }
+
+    private static COSDictionary getPDF2_0Namespace(PDStructureTreeRoot root) {
+        COSDictionary NS = new COSDictionary();
+        NS.setName("Type", "Namespace");
+        NS.setString("NS", "http://iso.org/pdf2/ssn");
+        COSArray namespaces = new COSArray();
+        namespaces.add(NS);
+        root.getCOSObject().setItem("Namespaces", namespaces);
+        return NS;
     }
 }
